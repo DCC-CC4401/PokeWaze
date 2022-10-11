3b462201aa06f014d0ff42b2e849892a330da722
MrChurrasco <sebastian.carrasco.olivero@gmail.com>
2022-10-11 05:50:00 -0300

    - Se pueden visualizar en la página las evoluciones y las otras formas de un Pokémon con redirección a su propia página.

--- PokeWaze/WikiDex/views.py
+++ PokeWaze/WikiDex/views.py
@@ -1,3 +1,4 @@
+from turtle import title
 from django.shortcuts import render
 from pathlib import Path
 import os
@@ -16,7 +17,6 @@ def obtener_pokemon(request:str)->render:
 
     Args:
         request (str): Link.
-        pkmn (str): Nombre de un Pokémon. 
 
     Returns:
         render: Genera la respuesta de la página Http con "Hello World".
@@ -24,35 +24,124 @@ def obtener_pokemon(request:str)->render:
     
     
     # Request
-    pkmn = request.GET["nameorid"]
-    
-    data_http = {} # Info a web
-    
+    pkmn_id = request.GET["nameorid"]
+     
     # Generamos datos para la página
     df = pd.read_csv(os.path.join(CVS_DIR,"pokemon.csv"))
     
     # Checkeemos si el usuario entregó la id en vez del nombre
-    if pkmn.isnumeric():
-        df_filter = df[df["id"]==int(pkmn)].reset_index().T
+    if pkmn_id.isnumeric():
+        df_filter = df[df["id"]==int(pkmn_id)].reset_index()
     else:
-        df_filter = df[df["identifier"]==pkmn.replace(" ","-").lower()].reset_index().T
+        df_filter = df[df["identifier"] == pkmn_id.replace(" ","-").lower()].reset_index()
     
     # si df_filter está vacío, el pokemon no es valido
     if df_filter.empty:
         return render(request=request,
                        template_name="404.html")
-    # Obtener data e imagen
-    data_pkmn = df_filter.to_dict()[0].items()
-    id_pkmn = list(data_pkmn)[1][1]
-    name_pkmn = list(data_pkmn)[2][1]
+        
+    # En caso que sea valido, crearemos un diccionario 
+    # de la información obtenida del Pokémon.
+    
+    data_pkmn = df_filter.reset_index().T.to_dict()[0]
+    
+    # Vaciamos memoria extra
+    del df
+    del df_filter
+    
+    # pkmn_id (int): ID del Pokémon.
+    pkmn_id = data_pkmn["id"] 
+    
+    # pkmn_name (str): Nombre del Pokémon.
+    pkmn_name = data_pkmn["identifier"].replace("-"," ")
+    pkmn_name = pkmn_name.title()
+    
+    # Obtener data útil del Pokémon
+    for x in ["level_0","identifier","index"]:
+        del data_pkmn[x]
+        
+    
+    pkmn = [(feature.replace("_"," ").title(),value) for feature, value in data_pkmn.items()]
+    
+    # list_evolutions (list(str)): Lista de Evoluciones del Pokémon.
+    
+    list_evolutions = query_evolutions(pkmn_id)
+    
+    # list_forms (list(str)): Lista de otras formas del Pokémon.
+    list_forms = list_evolutions["other"]
     
     # Agregamos las variables a traves de un diccionario al html
     # Subimos la página
     return render(request=request,
                   template_name="search.html",
-                  context={"pkmn":data_pkmn,
-                  "name":name_pkmn,
-                  "id":id_pkmn,
-                  "imageURL":f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{id_pkmn}.png"})
+                  context={"pkmn":pkmn,
+                  "name":pkmn_name,
+                  "formas": list_forms,
+                  "before": list_evolutions["before"],
+                  "after": list_evolutions["after"],
+                  "id":pkmn_id,
+                  "imageURL":f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{pkmn_id}.png"})
 
 
+############################## MÉTODOS AUXILIARES ###############################################
+
+def query_forms(pkmn_id:int)->list:
+
+    #Cargamos la base de datos a utilizar
+    df_pkmn = pd.read_csv(os.path.join(CVS_DIR,"pokemon.csv"))
+    df_pkmn_forms = pd.read_csv(os.path.join(CVS_DIR,"pokemon_forms.csv"))
+    
+    # Filtramos la información y la convertimos en listas
+    df_filter1 = df_pkmn[df_pkmn["species_id"] == pkmn_id]["identifier"].to_list()
+    df_filter2 = df_pkmn_forms[df_pkmn_forms["pokemon_id"] == pkmn_id]["identifier"].to_list()
+    
+    del df_pkmn_forms # Liberamos memoria
+    
+    # Unimos los identificadores de todas las formas de Pokémon buscado
+    list_forms = set(df_filter1+df_filter2)
+    list_forms.remove(df_pkmn[df_pkmn["id"]==pkmn_id]["identifier"].values[0])
+    
+    del df_pkmn, df_filter1, df_filter2 # Liberamos memoria
+    
+    return [(name.replace("-"," ").title(), identifier) for name,identifier in zip(list_forms,list_forms)]
+
+def query_evolutions(pkmn_id:int)->dict:
+    
+    #Cargamos la base de datos a utilizar
+    df_pkmn = pd.read_csv(os.path.join(CVS_DIR,"pokemon.csv"))
+    df_pkmn_spe = pd.read_csv(os.path.join(CVS_DIR,"pokemon_species.csv"))
+    
+    #Obtenemos el id de la especie del Pokémon
+    pkmn_spe_id = df_pkmn[df_pkmn["id"]==pkmn_id]["species_id"].values[0]
+    
+    #Obtenemos las otras formas
+    pkmn_other = df_pkmn[df_pkmn["species_id"]==pkmn_spe_id]["identifier"].to_list()
+    pkmn_other.remove(df_pkmn[df_pkmn["id"]==pkmn_id]["identifier"].values[0]) # Elimina al mismo Pokémon buscado
+    
+    del df_pkmn
+    
+    # Filtramos la información y la convertimos en listas
+    df_filter1 = df_pkmn_spe[df_pkmn_spe["id"] == pkmn_spe_id]["evolution_chain_id"]
+    df_filter2 = df_pkmn_spe[df_pkmn_spe["evolution_chain_id"] == df_filter1.values[0]]
+    
+    # Buscamos el order del Pokémon
+    order = df_pkmn_spe[df_pkmn_spe["id"]==pkmn_spe_id]["order"].values[0]
+    
+    del df_pkmn_spe, pkmn_spe_id
+    
+    #Obtenemos las evoluciones
+    pkmn_before = df_filter2[df_filter2["order"]<order]["identifier"].to_list()
+    pkmn_after = df_filter2[df_filter2["order"]>order]["identifier"].to_list()
+    
+    del df_filter1, df_filter2
+    
+    return {"before": [(name.replace("-"," ").title(), identifier) for name,identifier in zip(pkmn_before,pkmn_before)],
+            "after": [(name.replace("-"," ").title(), identifier) for name,identifier in zip(pkmn_after,pkmn_after)],
+            "other": set([(name.replace("-"," ").title(), identifier) for name,identifier in zip(pkmn_other,pkmn_other)]
+            + query_forms(pkmn_id=pkmn_id))
+            }    
+    
+    
+    del df_pkmn, df_filter1, df_filter2 # Liberamos memoria
+    
+    return [(name.replace("-"," ").title(), identifier) for name,identifier in zip(list_forms,list_forms)]
