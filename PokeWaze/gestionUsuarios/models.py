0217e6a6bd73c00ce804ed118a788a1ccd4f6f66
MrChurrasco <sebastian.carrasco.olivero@gmail.com>
2022-11-26 21:46:49 -0300

    Creación de los modelos provisorios para guardar Pokemon por Usuario.

--- PokeWaze/gestionUsuarios/models.py
+++ PokeWaze/gestionUsuarios/models.py
@@ -10,3 +10,30 @@ class Profile(models.Model):
 
   def __str__(self):
     return f"{self.user.username}'s profile"
+  
+class IdentifierNamePokemon(models.Model):
+  identifier = models.CharField(max_length=50, primary_key=True)
+  name = models.CharField(max_length=100)
+
+class TypePokemon(models.Model):
+  type = models.CharField(max_length=20, blank=True)
+  
+class Pokemon(models.Model):
+  identifier = models.ForeignKey(IdentifierNamePokemon,null=False,blank=False,on_delete=models.CASCADE)
+  id_species = models.IntegerField()
+  height = models.IntegerField()
+  weight = models.IntegerField()
+  hp = models.IntegerField()
+  attack = models.IntegerField()
+  sp_attack = models.IntegerField()
+  defense = models.IntegerField()
+  sp_defense = models.IntegerField()
+  speed = models.IntegerField()
+  type1_id = models.IntegerField()
+  type2_id = models.IntegerField()
+
+class Box(models.Model):
+  user_id = models.IntegerField()
+  pkmn_id = models.ForeignKey(Pokemon,null=False,blank=False, on_delete=models.CASCADE)
+  lvl_pkmn = models.IntegerField()
+  nickname_pkmn = models.CharField(max_length=50)
